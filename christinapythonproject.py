dae67c973b5d8605c65083dfcf3407815fcfb19b
Foster <CCF6703@yum.com>
2020-05-27 16:57:01 -0400

    Began initial code by adding importing csv file.

--- christinapythonproject.py
+++ christinapythonproject.py
@@ -1,1 +1,7 @@
-print ('hello world')
+import csv
+
+with open('Samplecomments2.csv') as csvfile:
+    commentreader = csv.DictReader(csvfile, delimiter=",")
+    rows = list(commentreader)
+    for row in rows[2:4]:
+        print(row['Comment'])
